1dc889e9f9101acecdaa8f8458d4336adc9ecd56
Waeiski <tuomas.vaisanen@helsinki.fi>
2018-11-29 13:52:54 +0200

    add option to create local time

--- spatial/location_history_creator.py
+++ spatial/location_history_creator.py
@@ -72,7 +72,32 @@ collist.extend(['location_hist'])
 # Join location history series to dataframe by user_id
 output_df = merged[collist]
 
+# Check whether local time zone was requested
+if args['localtime'] is not None:
+    print('[INFO] - Local time stamps requested!')
+    # Convert the datetime in the column 'time_created_utc' to local time
+    # (GMT+2). Begin by defining the source and target zones.
+    from_zone = pytz.timezone('UTC')
+    to_zone = pytz.timezone('Europe/Helsinki')  # change this to your timezone if needed
+    print('[INFO] - Creating local time stamps...')
+    # Loop over output dataframe
+    for ix, row in output_df.iterrows():
+        # Fetch the datetime
+        time_created_utc = row['time_created_utc']
+
+        # Tell the naive datetime that it's UTC
+        time_created_utc = time_created_utc.replace(tzinfo=from_zone)
+
+        # Convert the datetime to local timezone
+        time_created_local = time_created_utc.astimezone(to_zone)
+
+        # Add local time
+        output_df.at[ix, 'time_created_local'] = time_created_local
+else:
+    print('[INFO] - No local time stamps requested, moving on...')
+    pass
+
 # Save output dataframe as pickle
 print('[INFO] - Saving output dataframe to pickle')
 output_df.to_pickle(args['output'])
-
+print('[INFO] - ... Done!')
